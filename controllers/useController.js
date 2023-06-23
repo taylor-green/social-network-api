9aab6b49ff02edfa9063afa61238fe05151fb991
taylor-green <taylor.green2804@gmail.com>
2023-06-23 14:16:20 -0400

    added useController

---
+++ controllers/useController.js
@@ -0,0 +1,55 @@
+const { User, Application } = require('../models');
+
+module.exports = {
+  // get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // get single user by id
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // create new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // delete user and associated apps
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Application.deleteMany({ _id: { $in: user.applications } });
+      res.json({ message: 'User and associated applications deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
